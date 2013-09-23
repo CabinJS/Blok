e26be2628b2d8a93275939db3c382997b92cba31
ChrisWren <cthewren@gmail.com>
2013-09-23 09:16:30 -0700

    Added EJS support in cabin.json and integration test

--- test/testThemeInstallation.js
+++ test/testThemeInstallation.js
@@ -8,15 +8,20 @@ var wrench = require('wrench');
 
 describe('Blok theme', function () {
 
+  var EJSSiteName  = 'testEJSSite';
   var JadeSiteName = 'testJadeSite';
 
   before(function () {
+    if (fs.existsSync(EJSSiteName)) {
+      wrench.rmdirSyncRecursive(EJSSiteName);
+    }
     if (fs.existsSync(JadeSiteName)) {
       wrench.rmdirSyncRecursive(JadeSiteName);
     }
   });
 
   after(function () {
+    wrench.rmdirSyncRecursive(EJSSiteName);
     wrench.rmdirSyncRecursive(JadeSiteName);
   });
 
@@ -24,7 +29,29 @@ describe('Blok theme', function () {
   // rather than installing each time from npm
   var noInstall = process.env.NODE_ENV === 'dev' ? '--noInstall' : '';
 
-  it('should generate a site when installed with Cabin using the Jade template engine', function (done) {
+  it('should generate a site when installed with Cabin selecting the EJS template engine', function (done) {
+    var cabinProcess = spawn('node', ['node_modules/cabin/bin/cabin', 'new', EJSSiteName, '.', '--local', '--templateEngine', 'EJS', noInstall]);
+    cabinProcess.on('close', function () {
+
+
+      if (noInstall) {
+        wrench.copyDirSyncRecursive('node_modules', EJSSiteName + '/node_modules');
+      }
+
+      var gruntBuildProcess = spawn('grunt', ['build'], {
+        cwd: EJSSiteName
+      });
+
+      gruntBuildProcess.on('close', function () {
+        fs.existsSync(EJSSiteName + '/dist/index.html').should.eql(true, 'index.html created');
+        fs.existsSync(EJSSiteName + '/dist/posts/my-cool-blog-post/index.html').should.eql(true, 'my-cool-blog-post created');
+        fs.existsSync(EJSSiteName + '/dist/styles/main.css').should.eql(true, 'main.css created');
+        done();
+      });
+    });
+  });
+
+  it('should generate a site when installed with Cabin selecting the Jade template engine', function (done) {
     var cabinProcess = spawn('node', ['node_modules/cabin/bin/cabin', 'new', JadeSiteName, '.', '--local', '--templateEngine', 'Jade', noInstall]);
     cabinProcess.on('close', function () {
       if (noInstall) {
